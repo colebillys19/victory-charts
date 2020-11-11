5cdb223c94be1823cb4a96235f4dc05e384c0aee
Billy Cole <cole.billys@gmail.com>
2020-11-11 09:06:56 -0800

    add comment

--- src/App/index.js
+++ src/App/index.js
@@ -5,6 +5,7 @@ import { chartDict } from './constants';
 import { AppContainer, FlexContainer } from './styledComponents';
 
 const App = () => {
+  // string passed here determines which chart shows on page load (refer to chartDict constant)
   const [activeChart, setActiveChart] = useState('areaStackTemplate');
 
   return (
