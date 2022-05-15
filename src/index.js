a2db0ef5757c61a77e7464f1a09a924c0a32be92
Billy Cole <billycole@macbook-pro.mynetworksettings.com>
2022-05-15 18:16:16 -0400

    Remove service worker.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
 import App from './App/index';
 import './styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
