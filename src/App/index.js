154e469f20c72d0acb2807f3f250dca62194f083
Billy Cole <cole.billys@gmail.com>
2024-04-12 10:21:51 -0400

    Hide chart container when no chart selected.

--- src/App/index.js
+++ src/App/index.js
@@ -12,14 +12,17 @@ import {
 const App = () => {
   // string passed here determines which chart shows on page load (refer to chartDict constant)
   const [activeChart, setActiveChart] = useState('areaStackTemplate');
+  console.log(activeChart);
 
   return (
     <AppContainer className="App">
       <AppSubcontainer>
         <SelectMenu activeChart={activeChart} setActiveChart={setActiveChart} />
-        <ChartContainer paddingVal={getPaddingVal(activeChart)}>
-          {chartDict[activeChart]}
-        </ChartContainer>
+        {!!activeChart && activeChart !== '-' && (
+          <ChartContainer paddingVal={getPaddingVal(activeChart)}>
+            {chartDict[activeChart]}
+          </ChartContainer>
+        )}
       </AppSubcontainer>
     </AppContainer>
   );
